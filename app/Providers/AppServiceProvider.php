144e2088aa5f06e6949c4334b18f89db326dfb11
Debjit Biswas <debjit012@gmail.com>
2021-04-25 23:35:12 +0530

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+\URL::forceScheme('https');
         Blade::if('admin', function () {
             return auth()->check() && auth()->user()->isAdmin();
         });
